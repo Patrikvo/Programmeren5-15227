42d1918ed395cd4f49541fffaa09d0289b75a881
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2017-09-30 23:58:29 +0200

    EventTopicDDL.sql complete

---
+++ les4/EventTopicDDL.sql
@@ -0,0 +1,26 @@
+-- Globals instellen (alleen op je lokale MySQL Server, niet op die van de school)
+SET GLOBAL TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+SET GLOBAL sql_mode = 'ANSI';
+
+
+-- Database maken (alleen op je lokale MySQL Server, niet op die van de school) en activeren
+
+-- If database does not exist, create the database
+CREATE DATABASE IF NOT EXISTS docent1;
+USE docent1;
+
+--  Foreign Key constraints opheffen
+
+-- With the MySQL FOREIGN_KEY_CHECKS variable,
+-- you don't have to worry about the order of your
+-- DROP and CREATE TABLE statements at all, and you can
+-- write them in any order you like, even the exact opposite.
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `EventTopic`;
+CREATE TABLE `EventTopic` (
+`Name` NVARCHAR (120) NOT NULL,
+ `Id` INT NOT NULL AUTO_INCREMENT,
+    CONSTRAINT PRIMARY KEY(Id) );
+
+SET FOREIGN_KEY_CHECKS = 1;

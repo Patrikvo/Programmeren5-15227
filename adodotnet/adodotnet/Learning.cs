0f5c65348cce323cb20e868c286649188e029b36
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2017-10-08 21:28:09 +0200

    adodotnet volgens voorbeeld.

---
+++ adodotnet/adodotnet/Learning.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace AdoDotNet
+{
+    class Learning
+    {
+        public static void TestMySqlConnector()
+        {
+            MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection();
+            string ConnectionString = "server= x.x.x.x; user id=docent1;password=xx;port=3306;database=docent1;SslMode=none;";
+            connection.ConnectionString = ConnectionString;
+            Console.WriteLine("Connectie gemaakt.");
+            using (connection)
+            {
+                MySqlCommand command = new MySqlCommand();
+                string sqlStatement = "select Name, Id from EventCategory;";
+                command.Connection = connection;
+                command.CommandText = sqlStatement;
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader["Name"],
+                            reader["Id"]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
---
+++ adodotnet/adodotnet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdoDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leren werken met ADO.NET in .NET Core!");
+            Learning.TestMySqlConnector();
+        }
+    }
+}

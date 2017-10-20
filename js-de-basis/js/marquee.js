3464723b24c233c198dd00ce61c44558eb9a7083
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2017-10-20 21:41:03 +0000

    Enkele verbeteringen: scrollArea en marquee worden nu pas na de scrollDelay opgezocht in de DOM. Dit voorkomt een fout waarbij deze velden null zijn als de DOM nog niet (volledig) geladen was tijdens het opzoeken. Bij het pauzeren van het scrollen blijft de ingestelde snelheid bewaard.

--- js-de-basis/js/marquee.js
+++ js-de-basis/js/marquee.js
@@ -1,5 +1,6 @@
-var scrollDelay = 200; // Vertraging tot het scrollen begint.
+var scrollDelay = 2000; // Vertraging tot het scrollen begint.
 var marqueeSpeed = 1;    // hoe snel scrolled de tekst. hoger = sneller.
+var marqueePaused = false;
 var timer;
 
 var scrollArea;
@@ -7,38 +8,36 @@ var marquee;
 var scrollPosition = 0;
 
 var scrolling = function() {
-    if (scrollPosition + scrollArea.offsetHeight <= 0){
-        scrollPosition = marquee.offsetHeight;
+    if (!marqueePaused){
+        if (scrollPosition + scrollArea.offsetHeight <= 0){
+            scrollPosition = marquee.offsetHeight;
+        }
+        else{
+            scrollPosition -= marqueeSpeed;
+        }
+        scrollArea.style.top = scrollPosition + "px";
     }
-    else{
-        scrollPosition -= marqueeSpeed;
-    }
-    scrollArea.style.top = scrollPosition + "px";
 }
 
 var startScrolling = function(){
+    // scrollArea en marquee worden pas na de scrollDelay geladen, zodat de DOM zeker geladen is.
+    // Als de DOM nog niet geladen is, zijn scrollArea en marquee null en scrollt de lichtkrant niet.
+    scrollArea = document.getElementById("scroll-area");
+    scrollArea.style.top = 0;
+    
+    marquee = document.getElementById("marquee");
     
     timer = setInterval(scrolling, 30);
 }
 
 var initializeMarquee = function(){
-    
-    scrollArea = document.getElementById("scroll-area");
-    scrollArea.style.top = 0;
-    
-    marquee = document.getElementById("marquee");
     setTimeout(startScrolling, scrollDelay );
-   
 }
 
 // onthoud de gewijzigde snelheid niet.
 var pauseMarquee =  function(){
-    if (marqueeSpeed > 0){
-        marqueeSpeed = 0;
-    }
-    else{
-        marqueeSpeed = 1;
-    }
+    // door een boolean te gebruiken, wordt de snelheid bewaard tijdens het pauzeren.
+    marqueePaused = !marqueePaused;
 }
 
 var speedUpMarquee = function(){

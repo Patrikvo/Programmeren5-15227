e014e35b8963a6fd23659f43b0a873aa299aaacc
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2017-10-29 00:47:22 +0200

    map wordt geladen en gecentreerd op het doelgebied. markers worden getoond. Er verschijnen nog wel null-waarden in de marker-gegevens. Deze moeten nog gefilterd worden.

--- les8_map/map.js
+++ les8_map/map.js
@@ -1,3 +1,5 @@
+var sourceURL = "http://datasets.antwerpen.be/v4/gis/speelterreinen.json";
+
 // Set the map variable
 const myMap = L.map('map');
 
@@ -11,4 +13,38 @@ const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 myBasemap.addTo(myMap);
 
 // Set view of the map
-myMap.setView([41.939948, -87.650673], 12);
+myMap.setView([51.21208495916200, 4.39278641754120], 12);
+
+
+
+function getMarkers() {
+    const request = new XMLHttpRequest();
+    request.open('GET', sourceURL, true);
+
+    request.onload = function () {
+        
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+
+        const playgrounds = data.data.map(playground => {
+            L.marker([playground.point_lat, playground.point_lng]).bindPopup(`
+            <h2>${playground.naam}</h2>
+            <p><b>Adres:</b> ${playground.straat} ${playground.huisnummer} ${playground.postcode}  ${playground.district} </p>
+            <p><b>Aantal toestellen:</b> ${playground.toestellen}</p>
+            `).openPopup().addTo(myMap);
+        });
+
+
+    }
+
+    request.send();
+
+
+
+}
+
+
+
+
+
+window.onload = getMarkers();

cd0289f54537903b4e033b234acdd7f42825cfea
Patrik Van Ostaeyen <patrik.vanostaeyen@gmail.com>
2017-12-22 12:29:16 +0100

    fonts toegevoegd en constanten toegevoegd aan vos.js

--- cordova/vos-model/www/js/vos.js
+++ cordova/vos-model/www/js/vos.js
@@ -16,43 +16,50 @@ var vos = {
          */
         window.localStorage.clear();
         // zolang je aan het debuggen bent
-        localStorage.removeItem('model');
+        localStorage.removeItem(modelStorageName);
 
         // ga na of het model al geladen is
-        var model = JSON.parse(localStorage.getItem("model"));
+        var model = JSON.parse(localStorage.getItem(modelStorageName));
 
         if (model === null) {
-            $http('data/identity.json')
+            $http(identityFile)
                 .get()
                 .then(function (data) {
                     vos.model.identity = JSON.parse(data);
                     var payload = {};
                     // procedures depend on Role (in uppercase)
-                    var fileName = 'data/procedure' + vos.model.identity.role.toUpperCase() + '.json';
+                    var fileName = ProcedureFilePrefix + vos.model.identity.role.toUpperCase() + ProcedureFileSuffix;
                     return $http(fileName).get(payload);
                 })
                 .then(function (data) {
                     vos.model.procedureList = JSON.parse(data);
                     var payload = { 'id': 1 };
-                    return $http('data/position.json').get(payload);
+                    return $http(PositionFile).get(payload);
                 })
                 .then(function (data) {
                     vos.model.position = JSON.parse(data);
                     var payload = {};
-                    return $http('data/organisationList.json').get(payload);
+                    return $http(organisationListFile).get(payload);
                 })
                 .then(function (data) {
                     vos.model.organisationList = JSON.parse(data);
                     vos.model.loaded = true;
-                    localStorage.setItem('model', JSON.stringify(vos.model));
+                    localStorage.setItem(modelStorageName, JSON.stringify(vos.model));
                     // controller['home']['index'](); // de controller maken we later
                 })
                 .catch(function (data) {
                     vos.model.loaded = false;
-                    localStorage.setItem('model', JSON.stringify(vos.model));
+                    localStorage.setItem(modelStorageName, JSON.stringify(vos.model));
                 });
         } else {
             vos.model = model;
         }
     }
 }
+
+var identityFile = 'data/identity.json';
+var ProcedureFilePrefix = 'data/procedure';
+var ProcedureFileSuffix = '.json';
+var PositionFile = 'data/position.json';
+var organisationListFile = 'data/organisationList.json';
+var modelStorageName = 'model';
